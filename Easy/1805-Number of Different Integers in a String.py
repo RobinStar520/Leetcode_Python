f316d8e6b581c27e5519e37c4df91bcb1ee731e0
RobinStar520 <445579010@qq.com>
2024-03-22 12:45:19 +1030

    22/03/2024 update

---
+++ Easy/1805-Number of Different Integers in a String.py
@@ -0,0 +1,56 @@
+# You are given a string word that consists of digits and lowercase English letters.
+
+# You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+# Return the number of different integers after performing the replacement operations on word.
+
+# Two integers are considered different if their decimal representations without any leading zeros are different.
+
+ 
+
+# Example 1:
+
+# Input: word = "a123bc34d8ef34"
+# Output: 3
+# Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+# Example 2:
+
+# Input: word = "leet1234code234"
+# Output: 2
+# Example 3:
+
+# Input: word = "a1b01c001"
+# Output: 1
+# Explanation: The three integers "1", "01", and "001" all represent the same integer because
+# the leading zeros are ignored when comparing their decimal values.
+ 
+
+# Constraints:
+
+# 1 <= word.length <= 1000
+# word consists of digits and lowercase English letters.
+
+# Approach:
+# Use a queue.
+
+from collections import deque
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        q = deque()
+        number_set: set = set()
+        for ch in word:
+            if ch.isalpha():
+                if len(q) != 0:
+                    val: int = int("".join(q))
+                    number_set.add(val)
+                    q.clear()
+                else:
+                    continue
+            if ch.isnumeric():
+                q.append(ch)
+        if len(q) != 0:
+            val: int = int("".join(q))
+            number_set.add(val)
+        print(number_set)
+        return len(number_set)
